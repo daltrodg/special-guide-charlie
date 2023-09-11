9c8b9cd1d33b34fc760b0c96fdafb36ecaa3276b
daltrodg <142935745+daltrodg@users.noreply.github.com>
2023-09-11 16:01:05 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,25 @@
 $(document).ready(function () {
   $("#buffDoge").click(function () {
-    //code goes here
     console.log("position: ", $("#buffDoge").position());
   });
   
-  $("#weakDoge").click(function () {
-    console.log('position: ', $('#weakDoge').position());
-  });
-  
-
+//Draggable element. 
   $("#buffDoge").draggable({ 
     stop: function () {
-    //code goes here
   if ($("#buffDoge").position().left > 400 && $("#buffDoge").position().left < 650) 
       {
        console.log("test")
-        $("#buffDoge").attr(
-          "src",
-          "https://i.insider.com/52b86ce46bb3f7032e3faf9e?width=750&format=jpeg&auto=webp");
-     }  
+      }
     }
-   });
-  
-  $("#weakDoge").draggable({ 
-    stop: function () {
-    //code goes here
-  if ($("#weakDoge").position().left > 400 && $("#weakDoge").position().left < 650) 
-      {
-       console.log("test")
-        $("#weakDoge").attr(
-          "src",
-          "https://i.insider.com/52b86ce46bb3f7032e3faf9e?width=750&format=jpeg&auto=webp");
-     }  
+  });
+//Droppable element. 
+  $( "#cartoonGym" ).droppable({
+    drop: function()
+    {
+      console.log('test')
+      $("#cartoonGym").attr(
+      "src",
+      'https://resources.licensingprep.com/wp-content/uploads/2019/09/cheers-congratulations-meme.jpg');
     }
-   }); 
- });
+  });
+});
